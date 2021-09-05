8a1d76596d2c5c942a8cc11b0f0d0c1cb7ad67d2
Jeremy <jeremy.ling@unotech.io>
2021-09-05 10:06:41 +0800

    add auto external

--- rollup.config.js
+++ rollup.config.js
@@ -6,106 +6,135 @@ import json from "@rollup/plugin-json";
 import analyze from "rollup-plugin-analyzer";
 import pkg from "./package.json";
 import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+import autoExternal from "rollup-plugin-auto-external";
 
-const config = {
-  input: "src/index.js",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true,
-      globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
-        "mdi-material-ui": "mdiMaterialUi",
+const config = [
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
       },
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true,
-      globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
-        "mdi-material-ui": "mdiMaterialUi",
+      {
+        file: pkg.module,
+        format: "es",
+        exports: "named",
+        sourcemap: true,
       },
-    },
-    {
-      file: `dist/${pkg.name}.min.js`,
-      format: "umd",
-      name: "Editor",
-      exports: "named",
-      sourcemap: true,
-      globals: {
-        react: "React",
-        "react-dom": "ReactDOM",
-        "mdi-material-ui": "mdiMaterialUi",
+    ],
+    plugins: [
+      json(),
+      resolve(),
+      commonjs({
+        exclude: ["src/**"],
+        include: ["node_modules/**"],
+      }),
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+        plugins: [
+          [
+            "@babel/plugin-proposal-decorators",
+            {
+              legacy: true,
+            },
+          ],
+          "@babel/plugin-proposal-function-sent",
+          "@babel/plugin-proposal-export-namespace-from",
+          "@babel/plugin-proposal-numeric-separator",
+          "@babel/plugin-proposal-throw-expressions",
+          [
+            "transform-react-remove-prop-types",
+            {
+              removeImport: true,
+            },
+          ],
+        ],
+        presets: ["@babel/react", "@babel/env"],
+        comments: false,
+      }),
+      analyze({ summaryOnly: true, limit: 10 }),
+      sizeSnapshot(),
+      terser(),
+      autoExternal(),
+    ],
+    external: ["lodash/isEqual", "@material-ui/core/styles", "clsx"],
+  },
+  {
+    input: "src/index.js",
+    output: [
+      {
+        file: `dist/${pkg.name}.min.js`,
+        format: "umd",
+        name: "Editor",
+        exports: "named",
+        sourcemap: true,
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "mdi-material-ui": "mdiMaterialUi",
+        },
       },
-    },
-  ],
-  plugins: [
-    json(),
-    resolve(),
-    commonjs({
-      exclude: ["src/**"],
-      include: ["node_modules/**"],
-    }),
-    babel({
-      babelHelpers: "bundled",
-      exclude: "node_modules/**",
-      plugins: [
-        [
-          "@babel/plugin-proposal-decorators",
-          {
-            legacy: true,
-          },
+    ],
+    plugins: [
+      json(),
+      resolve(),
+      commonjs({
+        exclude: ["src/**"],
+        include: ["node_modules/**"],
+      }),
+      babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+        plugins: [
+          [
+            "@babel/plugin-proposal-decorators",
+            {
+              legacy: true,
+            },
+          ],
+          "@babel/plugin-proposal-function-sent",
+          "@babel/plugin-proposal-export-namespace-from",
+          "@babel/plugin-proposal-numeric-separator",
+          "@babel/plugin-proposal-throw-expressions",
+          [
+            "transform-react-remove-prop-types",
+            {
+              removeImport: true,
+            },
+          ],
+          "babel-plugin-lodash",
+          [
+            "babel-plugin-import",
+            {
+              libraryName: "@material-ui/core",
+              libraryDirectory: "esm",
+              camel2DashComponentName: false,
+            },
+            "core",
+          ],
+          [
+            "babel-plugin-import",
+            {
+              libraryName: "@material-ui/icons",
+              libraryDirectory: "esm",
+              camel2DashComponentName: false,
+            },
+            "icons",
+          ],
         ],
-        "@babel/plugin-proposal-function-sent",
-        "@babel/plugin-proposal-export-namespace-from",
-        "@babel/plugin-proposal-numeric-separator",
-        "@babel/plugin-proposal-throw-expressions",
-        [
-          "transform-react-remove-prop-types",
-          {
-            removeImport: true,
-          },
-        ],
-        "babel-plugin-lodash",
-        [
-          "babel-plugin-import",
-          {
-            libraryName: "@material-ui/core",
-            libraryDirectory: "esm",
-            camel2DashComponentName: false,
-          },
-          "core",
-        ],
-        [
-          "babel-plugin-import",
-          {
-            libraryName: "@material-ui/icons",
-            libraryDirectory: "esm",
-            camel2DashComponentName: false,
-          },
-          "icons",
-        ],
-      ],
-      presets: ["@babel/react", "@babel/env"],
-      comments: false,
-    }),
-    analyze({ summaryOnly: true, limit: 10 }),
-    sizeSnapshot(),
-    terser(),
-  ],
-  external: [
-    "react",
-    "react-dom",
-    "@material-ui/core",
-    "lodash",
-    "mdi-material-ui",
-  ],
-};
+        presets: ["@babel/react", "@babel/env"],
+        comments: false,
+      }),
+      analyze({ summaryOnly: true, limit: 10 }),
+      sizeSnapshot(),
+      terser(),
+    ],
+    external: ["react", "react-dom"],
+  },
+];
 
 export default config;
